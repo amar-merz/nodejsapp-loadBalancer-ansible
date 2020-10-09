33f3def5cd8a48d291e4b172167779eeeb9e2a33
Amar Merzougui <46235692+amar-merz@users.noreply.github.com>
2020-10-09 15:59:14 -0400

    Update deploy.sh

--- nodejsapp_demo/deploy.sh
+++ nodejsapp_demo/deploy.sh
@@ -2,10 +2,10 @@
 npm install
 
 
-ssh -i /home/ammar/.ssh/id_rsa ubuntu@3.15.240.208 'sudo mkdir -p /app; sudo chown -R ubuntu /app'
-rsync -avz -e "ssh -i /home/ammar/.ssh/id_rsa" ./ ubuntu@3.15.240.208:/app/
-ssh -i /home/ammar/.ssh/id_rsa ubuntu@3.15.240.208 "sudo kill node;cd /app;node index.js > output.log 2>&1 &"
+ssh -i $HOME/user/.ssh/file_keyname user@x.xx.xxx.xxx 'sudo mkdir -p /app; sudo chown -R user /app'
+rsync -avz -e "ssh -i $HOME/user/.ssh/file_keyname" ./ ubuntu@x.xx.xxx.xxx :/app/
+ssh -i $HOME/user/.ssh/file_keyname user@x.xx.xxx.xxx  "sudo kill node;cd /app;node index.js > output.log 2>&1 &"
 
-ssh -i /home/ammar/.ssh/id_rsa ubuntu@18.222.54.77 "sudo mkdir -p /app;sudo chown -R ubuntu /app"
-rsync -avz -e "ssh -i /home/ammar/.ssh/id_rsa" ./ ubuntu@18.222.54.77:/app/
-ssh -i /home/ammar/.ssh/id_rsa ubuntu@18.222.54.77 "sudo pkill node;cd /app;node index.js > output.log 2>&1 &"
+ssh -i $HOME/user/.ssh/file_keyname user@x.xx.xxx.xxx  "sudo mkdir -p /app;sudo chown -R user /app"
+rsync -avz -e "ssh -i $HOME/user/.ssh/file_keyname" ./ ubuntu@x.xx.xxx.xxx :/app/
+ssh -i $HOME/user/.ssh/file_keyname user@x.xx.xxx.xxx  "sudo pkill node;cd /app;node index.js > output.log 2>&1 &"
