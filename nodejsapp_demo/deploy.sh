317bb9f3b4450665e90bad5be5e33a5ae9cc91ec
Amar Merzougui <46235692+amar-merz@users.noreply.github.com>
2020-10-10 10:31:59 -0400

    Update deploy.sh

--- nodejsapp_demo/deploy.sh
+++ nodejsapp_demo/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash/
 npm install
 
+# The script is not for using in the prod
 
 ssh -i $HOME/user/.ssh/file_keyname user@x.xx.xxx.xxx 'sudo mkdir -p /app; sudo chown -R user /app'
 rsync -avz -e "ssh -i $HOME/user/.ssh/file_keyname" ./ ubuntu@x.xx.xxx.xxx :/app/
